b1f473c09d47a1bf94df1129520d2c3dac581c6d
Alok Ranjan <aloks@byteridge.com>
2023-04-28 19:09:38 +0530

    Upgraded to webpack v5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import './index.scss';
+import "./index.scss";
 
-const p = document.createElement('p');
-p.textContent = 'Welcome to webpack world served using dev server alok!!';
+const p = document.createElement("p");
+p.textContent = "Welcome to webpack world served using dev server!!";
 document.body.appendChild(p);
 
-const p2 = document.createElement('p');
+const p2 = document.createElement("p");
 const numbers1 = [1, 2, 3, 4, 5, 6];
 const numbers2 = [7, 8, 9, 10];
 const numbers3 = [...numbers1, ...numbers2];
-p2.textContent = numbers3.join(' ');
+p2.textContent = numbers3.join(" ");
 document.body.appendChild(p2);
